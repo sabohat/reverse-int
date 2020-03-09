147862d5ee39b1401e0f824512e68c4a2a975929
Sabokhat Kalandarova <sabohat2802@gmail.com>
2020-03-09 09:29:45 +0500

    feat: wrote the test for the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+   if(n>=0){
+        myStr = n.toString();
+}else {
+    myStr =  Math.abs(n).toString();
+}
+myNum = myStr.split('').reverse().join('');
+rever = Number(myNum);
+ return rever;
+      
 }
